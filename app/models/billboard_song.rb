e92ec57835ce9c1e9c2f633a69cec09cc711ba88
Luke Thomas <lukert33@gmail.com>
2015-02-25 10:28:24 -0500

    Constrain uniqueness on factoids.

--- app/models/billboard_song.rb
+++ app/models/billboard_song.rb
@@ -1,4 +1,5 @@
 class BillboardSong < ActiveRecord::Base
   include Factoid
   validates :track, :artist, presence: true
+  validates :track, uniqueness: true
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,6 @@
 class Quote < ActiveRecord::Base
   include Factoid
-  validates :excerpt, presence: true
+  validates :excerpt, presence: true, uniqueness: true
   before_save :fill_author
 
   def fill_author
