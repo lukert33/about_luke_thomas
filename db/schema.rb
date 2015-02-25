1d7de369e79a3c559f5893884812867f9318a83b
Luke Thomas <lukert33@gmail.com>
2015-02-24 19:10:41 -0500

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224234003) do
+ActiveRecord::Schema.define(version: 20150224235923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,11 @@ ActiveRecord::Schema.define(version: 20150224234003) do
   end
 
   create_table "ratios", force: :cascade do |t|
-    t.float    "number"
+    t.string   "number"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "numfloat"
   end
 
 end
