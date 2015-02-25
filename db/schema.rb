09052a61a7e21736452566529b3bc2a015644189
Luke Thomas <lukert33@gmail.com>
2015-02-24 19:20:53 -0500

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224235923) do
+ActiveRecord::Schema.define(version: 20150225001202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "billboard_songs", force: :cascade do |t|
+    t.string   "track",      null: false
+    t.string   "artist",     null: false
+    t.integer  "day"
+    t.string   "month"
+    t.integer  "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.string   "author"
