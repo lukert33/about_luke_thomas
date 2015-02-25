a386c989c163b6fea7c52ef0511723c49d79e522
Luke Thomas <lukert33@gmail.com>
2015-02-25 10:25:36 -0500

    Provide more randomization for view.

--- app/controllers/factoids_controller.rb
+++ app/controllers/factoids_controller.rb
@@ -2,9 +2,11 @@ class FactoidsController < ApplicationController
 
   def index
     @quote    = Quote.random_instance
-    @ratio_a  = Ratio.random_instance
-    @ratio_b  = other_rand_ratio(@ratio_a)
+    ratio_a   = Ratio.random_instance
+    ratio_b   = other_rand_ratio(ratio_a)
+    @ratios   = [ratio_a, ratio_b]
     @song     = BillboardSong.random_instance
+    @flip     = rand(2)
 
     render "index"
   end
