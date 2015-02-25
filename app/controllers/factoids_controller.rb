3540838e5917c27b15c6a04a0ab93fb6ad68b975
Luke Thomas <lukert33@gmail.com>
2015-02-25 09:10:26 -0500

    Define empty controller actions.

---
+++ app/controllers/factoids_controller.rb
@@ -0,0 +1,18 @@
+class FactoidsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    #admin only
+  end
+
+  def create
+    #admin only
+  end
+
+  def destroy
+    #admin only
+  end
+
+end
