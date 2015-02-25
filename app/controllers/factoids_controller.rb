82d5cb9584a1fc30147597bd61b9a20e84ac704d
Luke Thomas <lukert33@gmail.com>
2015-02-25 09:59:17 -0500

    Serve instance methods w/ several random ratios.

--- app/controllers/factoids_controller.rb
+++ app/controllers/factoids_controller.rb
@@ -1,10 +1,12 @@
 class FactoidsController < ApplicationController
 
   def index
-    quote = Quote.random_instance
-    ratio = Ratio.random_instance
-    song  = BillboardSong.random_instance
-    render text: [quote.excerpt, ratio.description, song.track]
+    @quote    = Quote.random_instance
+    @ratio_a  = Ratio.random_instance
+    @ratio_b  = other_rand_ratio(@ratio_a)
+    @song     = BillboardSong.random_instance
+
+    render "index"
   end
 
   def new
@@ -17,6 +19,16 @@ class FactoidsController < ApplicationController
 
   def destroy
     #admin only
+  end
+
+  private
+
+  def other_rand_ratio(ratio)
+    other_ratio = Ratio.random_instance
+    until other_ratio != ratio
+      other_ratio = Ratio.random_instance
+    end
+    other_ratio
   end
 
 end
