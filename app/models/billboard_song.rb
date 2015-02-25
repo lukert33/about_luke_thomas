1ffa7e5309d808fc44b4bf97d566b5ef6186c01e
Luke Thomas <lukert33@gmail.com>
2015-02-25 13:34:13 -0500

    Sanitize song fields given scraped data.

--- app/models/billboard_song.rb
+++ app/models/billboard_song.rb
@@ -2,4 +2,16 @@ class BillboardSong < ActiveRecord::Base
   include Factoid
   validates :track, :artist, presence: true
   validates :track, uniqueness: true
+  before_save :format_strings
+
+  def format_strings
+    remove_cruff!(track)
+    remove_cruff!(artist)
+  end
+
+  def remove_cruff!(string)
+    string.gsub!(/^\s+/, "")
+    string.gsub!(/\s+$/, "")
+    string.gsub!(/\n/, "")
+  end
 end
