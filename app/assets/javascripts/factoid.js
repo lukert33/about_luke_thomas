3d5ffa891cc11b2225f4e8c8f858cc87787737c9
Luke Thomas <lukert33@gmail.com>
2015-02-26 14:45:13 -0500

    Rotate button on ajax call.

--- app/assets/javascripts/factoid.js
+++ app/assets/javascripts/factoid.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
 
-  $("body").on("click", "#new-facts button", function(event){
-    event.preventDefault();
+  var refreshable = true;
 
-    $.ajax({
-      url: "/factoids",
-      type: "get"
-    }).done(function(resp){
-      $("#factoid-display").empty().append(resp);
-    }).fail(function(respo){
-      console.log(Error("Couldn't reload facts"));
+  if(refreshable === true){
+    $("body").on("click", "#new-facts button", function(event){
+      event.preventDefault();
+      refreshable = false;
+      $("#new-facts").attr("style", "");
+
+      $.ajax({
+        url: "/factoids",
+        type: "get"
+      }).always(function(){
+        $("#new-facts").transition({
+          rotate: '360deg',
+          duration: 600
+        });
+      }).done(function(resp){
+        $("#factoid-display").empty().append(resp);
+        refreshable = true;
+      }).fail(function(respo){
+        console.log(Error("Couldn't reload facts"));
+        refreshable = true;
+      });
     });
-  });
-
+  }
 });
