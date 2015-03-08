fee5e84cd8b0140b4384409ce970d39d2984559e
Luke Thomas <lukert33@gmail.com>
2015-03-08 15:43:32 -0400

    Move refreshable condition into event listener.

--- app/assets/javascripts/factoid.js
+++ app/assets/javascripts/factoid.js
@@ -2,27 +2,30 @@ $(document).ready(function(){
 
   var refreshable = true;
 
-  if(refreshable === true){
+
     $("body").on("click", "#new-facts button", function(event){
       event.preventDefault();
-      refreshable = false;
-      $("#circle-arrow").attr("style", "");
 
-      $.ajax({
-        url: "/factoids",
-        type: "get"
-      }).always(function(){
-        $("#circle-arrow").transition({
-          rotate: '360deg',
-          duration: 600
+      if(refreshable === true){
+        refreshable = false;
+        $("#circle-arrow").attr("style", "");
+
+        $.ajax({
+          url: "/factoids",
+          type: "get"
+        }).always(function(){
+          $("#circle-arrow").transition({
+            rotate: '360deg',
+            duration: 600
+          });
+        }).done(function(resp){
+          $("#factoid-display").empty().append(resp);
+          refreshable = true;
+        }).fail(function(respo){
+          console.log(Error("Couldn't reload facts"));
+          refreshable = true;
         });
-      }).done(function(resp){
-        $("#factoid-display").empty().append(resp);
-        refreshable = true;
-      }).fail(function(respo){
-        console.log(Error("Couldn't reload facts"));
-        refreshable = true;
-      });
+      }
     });
-  }
+
 });
